c347ceb3e18cd43342a7f9e984db7c0a39f1c2b0
evanalifian <evanalifian@gmal.com>
2024-10-22 19:31:18 +0700

    create first link profile project

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,96 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./preflight.css";
+import "./style.css";
+import "./utilities.css";
+import data from "../data.json";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <main>
+        <header className="profile container">
+          <figure className="profile_header">
+            <img
+              src="./public/img/evan.png"
+              alt="Evan Alifian"
+              width="112"
+              loading="lazy"
+              className="profile_header_image"
+            />
+            <figure className="profile_header_title">
+              <span className="profile_header_title_name">Evan Alifian</span>
+              <span className="profile_header_title_catchword">
+                I make code with JavaScript.
+              </span>
+              <span></span>
+            </figure>
+          </figure>
+        </header>
+        <main className="content container">
+          <section className="content_about">
+            <h1>About Me</h1>
+            <p>
+              Hi! my name is Evan Rafa Radya Alifian. I am a student at
+              Trunojoyo Madura University. I have a basic experince in web
+              development, especially in frontend development. Currently, i
+              build website with
+              <span className="badge">HTML5</span>,
+              <span className="badge">JavaScript</span>,
+              <span className="badge">TailwindCSS</span>, and
+              <span className="badge">React.js</span>.
+            </p>
+          </section>
+          <section className="content_skills">
+            <h2>Skills</h2>
+            <ul>
+            {data.skills.map((skill) => (
+              <li key={skill.name}>
+                <figure className="social_card">
+                  <img src={skill.icon} alt={skill.name} width="28" loading="lazy" className="social_card_image" />
+                  <figcaption className="social_card_title">
+                    <span className="social_card_title_name">{skill.name}</span>
+                  </figcaption>
+                </figure>
+              </li>
+            ))}
+            </ul>
+          </section>
+          <section className="content_social">
+            <h2>Social Media</h2>
+            <ul>
+              {data.social_media.map((data, i) => (
+                <li key={i}>
+                  <a href="${data.url}" target="_blnank">
+                    <figure className="social_card">
+                      <img
+                        src={data.icon}
+                        alt={data.name}
+                        width="44"
+                        loading="lazy"
+                        className="social_card_image"
+                      />
+                      <figcaption className="social_card_title">
+                        <span className="social_card_title_name">{data.name}</span>
+                        <span className="social_card_title_username">
+                          @{data.username}
+                        </span>
+                      </figcaption>
+                    </figure>
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </section>
+        </main>
+      </main>
+      <footer className="container">
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          If you need something from me, you can email me on{" "}
+          <a href="mailto:evanalifian@gmail.com">evanalifian@gmail.com</a>
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <span>&copy; 2024 evanalifian</span>
+      </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
