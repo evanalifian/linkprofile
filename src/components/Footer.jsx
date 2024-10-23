c657c92c87d72e7b4935a08d4c09639460fe7040
evanalifian <evanalifian@gmal.com>
2024-10-23 12:25:41 +0700

    resize text on footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,18 +2,18 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="text-center mt-auto">
+    <footer className="text-center mt-auto text-sm">
       <div className="max-w-xl mx-auto p-4">
         <p>
           If you need something from me, you can email me on{" "}
           <a
             href="mailto:evanalifian@gmail.com"
-            className="text-gray-700 transition-colors hover:text-gray-900"
+            className="text-gray-700 font-medium transition-colors hover:text-gray-900"
           >
             evanalifian@gmail.com
           </a>
         </p>
-        <span className="text-sm">&copy; 2024 evanalifian</span>
+        <span>&copy; 2024 evanalifian</span>
       </div>
     </footer>
   );
